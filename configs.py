dfe78ddfc0c44f67795a33f0f67c8cf09867b89c
ilcyb <hybmail1996@gmail.com>
2018-03-22 09:04:40 +0800

    add configs.py

---
+++ configs.py
@@ -0,0 +1,11 @@
+all_configs = ('aria2_path', 'aria2_rpc_hostname', 'aria2_rpc_port', 'aria2_rpc_secret')
+
+need_read_configs = ('save_path', 'plugins_path')
+
+aria2_path = 'aria2c' # default path of aria2
+
+aria2_rpc_hostname = 'http://127.0.0.1' # default hostname of aria2c-rpc server
+
+aria2_rpc_port = 6800 # default port of aria2c-rpc server
+
+aria2_rpc_secret = '!@fsd./23' # default secret token of aria2c-rpc server
