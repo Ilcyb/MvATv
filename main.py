1f3e77d0ff92d7c8bbe3d8fd292d6e5e93b679eb
ilcyb <hybmail1996@gmail.com>
2018-03-23 16:59:49 +0800

    test data source

---
+++ main.py
@@ -0,0 +1,9 @@
+from mvatv.plugin.data_sources.zimuzu import ZIMUZU
+from mvatv.utils.utils import Quality
+import traceback
+
+z = ZIMUZU()
+try:
+    print(z._search('闪电侠', 4, 1, Quality.Low, False))
+except Exception as e:
+    print(traceback.format_exc())
